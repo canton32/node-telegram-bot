f19f8a75904283e7769b4385967357a108e4499c
Anton <antonverhoeven32@gmail.com>
2021-03-26 06:39:09 -0700

    implement a simple command

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
+const Clear = require("codeday-clear");
+const moment = require("moment");
 
 const telegram = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
   polling: true,
 });
 
+const clear = new Clear(
+  "1YZiGaj3baaLU8IKVsASRIWaNF2oJNg0",
+  "1COMnWyGnGBsNqkhaZ6WMBWB9UWZw6QZ"
+);
+
 telegram.on("text", (message) => {
-  telegram.sendMessage(message.chat.id, "Hello world");
+  if (message.text.toLowerCase().indexOf("/codeday") === 0) {
+    clear.getEventById("oo4QIuKQQTYA", (codedayEvent) => {
+      var endsAt = moment(codedayEvent.ends_at * 1000);
+      telegram.sendMessage(
+        message.chat.id,
+        "CodeDay ends " + endsAt.fromNow() + "!"
+      );
+    });
+  }
 });
